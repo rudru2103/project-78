fd4c186a06efa762c8cf1c00be4c722778307df7
rudru2103 <92202864+rudru2103@users.noreply.github.com>
2024-11-04 11:43:01 +0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var i = 0;
 function update()
 {
     i++;
-    var numbers_of_family_member_in_array = 5
+    var numbers_of_family_member_in_array = 4
     if(i > numbers_of_family_member_in_array)
       {
           i = 0;
