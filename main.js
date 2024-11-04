ea2b2d982d28182e6d14c58a54c80ab598df8162
rudru2103 <92202864+rudru2103@users.noreply.github.com>
2024-11-04 11:41:25 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg" , "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Family Book","Siddarth Khaturia", "Rudra Khaturia", "Alia Khaturia", "Kavya Khaturia"];
 var i = 0;
 function update()
 {
